a00ba971abd5ad4be5d140f5690f92e2edbad7eb
Temporal Data (cicd) <commander-data@temporal.io>
2025-07-01 18:28:36 +0000

    Sync from UI release v2.37.7

--- server/version/version.go
+++ server/version/version.go
@@ -28,5 +28,5 @@ const (
 	clientNameHeaderName    = "client-name"
 	clientNameHeaderValue   = "temporal-ui"
 	clientVersionHeaderName = "client-version"
-	UIVersion               = "2.37.6"
+	UIVersion               = "2.37.7"
 )
---
+++ ui/assets/local/_app/immutable/entry/start.l5ToC4UV.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/KLF1n_KR.js";export{o as load_css,r as start};
