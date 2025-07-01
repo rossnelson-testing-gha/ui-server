91d48045282939fadbf8fde174d0e527e310b9e6
Temporal Data (cicd) <commander-data@temporal.io>
2025-07-01 18:25:32 +0000

    Sync from UI release v2.37.6

--- server/version/version.go
+++ server/version/version.go
@@ -28,5 +28,5 @@ const (
 	clientNameHeaderName    = "client-name"
 	clientNameHeaderValue   = "temporal-ui"
 	clientVersionHeaderName = "client-version"
-	UIVersion               = "2.37.5"
+	UIVersion               = "2.37.6"
 )
